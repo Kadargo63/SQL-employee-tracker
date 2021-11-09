2ef4fe6437e3a1b827c96f79810e27fcf45b1e79
Robert Isaacs <robertisaacs87@yahoo.com>
2021-11-09 17:30:24 -0600

    started index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require('console.table');
+
+const promptMessages = {
+    viewAllEmployees: "View All Employees",
+    viewByDepartment: "View All Employees By Department",
+    viewByManager: "View All Employees By Manager",
+    addEmployee: "Add An Employee",
+    removeEmployee: "Remove An Employee",
+    updateRole: "Update Employee Role",
+    updateEmployeeManager: "Update Employee Manager",
+    viewAllRoles: "View All Roles",
+    exit: "Exit"
+};
+
+const conne
